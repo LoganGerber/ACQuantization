0b8c5df55048f77480439cd229b04c6e8baa3e68
Logan G <logangerber1014@yahoo.com>
2020-03-28 05:12:16 -0700

    Fix code formatting

--- ACColorGenerator.py
+++ ACColorGenerator.py
@@ -1,40 +1,42 @@
+"""Get all valid RGB colors in the Animal Crossing: New Horizon Custom Designer."""
 import colorsys
-import sys
 import argparse
-
-SAT_VAL_STEP = 100 / 14
 
 
 def GenerateValidACColors():
+    """ Generate a set of all unique RGB colors available for use in the Animal Crossing: New Horizon Custom Designer.
+    """
+    sat_val_step = 100 / 14
+
     rgb_values = set()
-    for h in range(0, 360, 12):
-        s = 0
-        while s <= 100:
-            v = 0
-            while v <= 100:
-                rgb = colorsys.hsv_to_rgb(h / 360, s / 100, v / 100)
+    for hue in range(0, 360, 12):
+        sat = 0
+        while sat <= 100:
+            val = 0
+            while val <= 100:
+                rgb = colorsys.hsv_to_rgb(hue / 360, sat / 100, val / 100)
                 rgb = tuple(map(lambda x: int(x * 255), rgb))
 
                 rgb_values.add(rgb)
 
-                v += SAT_VAL_STEP
-            s += SAT_VAL_STEP
+                val += sat_val_step
+            sat += sat_val_step
 
     return rgb_values
 
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser(
-        description='Print all valid RGB colors in Animal Crossing: New Horizon.', prefix_chars='-/')
+        description='Print all valid RGB colors in Animal Crossing: New Horizon Custom Designer.', prefix_chars='-/')
     argparser.add_argument('-t', '--total', action='store_true', dest='print_total',
                            help='Print the total number of entries at the end.')
 
     args = argparser.parse_args()
 
-    vals = GenerateValidACColors()
+    colors = GenerateValidACColors()
 
-    for val in vals:
-        print(val)
+    for color in colors:
+        print(color)
 
     if args.print_total:
-        print('{} total values'.format(len(vals)))
+        print('{} total colors.'.format(len(colors)))
